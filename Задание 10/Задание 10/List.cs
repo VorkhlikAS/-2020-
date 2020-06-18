a92b557273f13cc35c15e38338ac18fb582cb447
VorkhlikAS <63171045+VorkhlikAS@users.noreply.github.com>
2020-06-18 18:41:02 +0500

    Add files via upload

---
+++ Задание 10/Задание 10/List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_10
+{
+    class List
+    {
+        public double number;
+        public List next;
+
+        public List(double val)
+        {
+            number = val;
+        }
+        public void Show()
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ Задание 10/Задание 10/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_10
+{
+    class Program
+    {
+        static double GetDouble()
+        {
+            bool ok = false;
+            double temp = 0;
+            do
+            {
+                try
+                {
+                    temp = Convert.ToDouble(Console.ReadLine());
+                    ok = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите действительное число");
+                }
+            } while (!ok);
+            return temp;
+        }
+        static List Itteration(int n)
+        {
+            if (n == 0) return null;
+            else
+            {
+                double val = GetDouble();
+                List vectorN = new List(val);
+                vectorN.next = Itteration(n - 1);
+                return vectorN;
+            }
+        }
+        static void ShowVector(List vector)
+        {
+            if (vector != null)
+            {
+                vector.Show();
+                ShowVector(vector.next);
+            }
+
+        }
+        static bool CheckNotNegativeIncrease(List vector)
+        {
+            if (vector.next != null)
+            {
+                if (vector.number <= vector.next.number) return CheckNotNegativeIncrease(vector.next);
+                else return false;
+            }
+            else return true;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            bool ok = false;
+            int n = 0;
+            do
+            {
+                try
+                {
+                    n = Convert.ToInt32(Console.ReadLine()); //Ввод числа n
+                    ok = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            } while (!ok);
+            if (n != 0)
+            {
+                Console.WriteLine("Введите члены последовательности");
+                List vector = Itteration(n);
+                Console.WriteLine("---");
+
+                if (!CheckNotNegativeIncrease(vector))
+                {
+                    List temp = new List(vector.number);
+                    vector = ShaffleVector(vector.next, temp);
+                }
+
+                ShowVector(vector);
+                Console.WriteLine("---");
+
+                Console.ReadLine();
+            }
+        }
+        static List ShaffleVector(List vector, List prev)
+        {
+
+            if (!(vector == null))
+            {
+                List temp = new List(vector.number);
+                temp.next = prev;
+                return ShaffleVector(vector.next, temp);
+            }
+            else return prev;
+        }
+    }
+}
